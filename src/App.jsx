a826652a9e07fdcb8a7143c3677dfdf09ed51a0d
mandar-saiyan87 <mdman2257@gmail.com>
2024-04-01 22:44:28 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-400 text-center text-lg font-Poppins'>
+        React Vite Project
+      </div>
+
+    </>
+  )
+}
+
+export default App
